e904f232a05204da702359cc344b172d68eab8a8
GerritStoecker <gerrit.stoecker@gmx.de>
2015-02-04 08:35:48 +0100

    Initial commit

---
+++ GitTestProject/src/GitTestClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author gerrit.stoecker
+ *
+ */
+public class GitTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
